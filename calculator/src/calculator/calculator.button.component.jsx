23817508ed0ca6332b4fff13d22c90663fdc3d85
jogieglo <jogieglo@future-processing.cm>
2018-11-14 22:18:46 +0100

    Fixed button alignment and display

--- calculator/src/calculator/calculator.button.component.jsx
+++ calculator/src/calculator/calculator.button.component.jsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export default props => {
   return (
-    <div class="col-auto mr-auto border border-primary">{props.value}</div>
+    <div class="col border border-primary text-center">{props.value}</div>
   );
 };
